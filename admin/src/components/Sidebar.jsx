9afd0f6e3e0be8299da9735688ded4c4937e332a
david-malviya <davidmalviya2004@gmail.com>
2025-01-21 23:16:34 +0530

    new files of graphs

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { AdminContext } from '../context/AdminContext'
 import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { DoctorContext } from '../context/DoctorContext'
+import { FaChartLine } from "react-icons/fa6";
 
 const Sidebar = () => {
 
@@ -28,6 +29,12 @@ const Sidebar = () => {
                 </NavLink>
                 <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer  ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/doctor-list'}>
                     <img src={assets.people_icon} alt="" />
+                    <p>Doctors List</p>
+                </NavLink>
+
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer  ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}` } to={'/doctor-list'}>
+                    {/* <img src={assets.people_icon} alt="" /> */}
+                    <FaChartLine />
                     <p>Doctors List</p>
                 </NavLink>
             </ul>
